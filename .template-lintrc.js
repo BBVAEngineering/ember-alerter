b1b047086405e5e0054f88207b40f80108ff6f89
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2019-04-05 11:47:11 +0200

    style: fix some lint errors

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+	extends: 'recommended',
+	rules: {
+		'block-indentation': 'tab',
+		'deprecated-each-syntax': true,
+		'deprecated-inline-view-helper': true,
+		'img-alt-attributes': true,
+		'inline-link-to': true,
+		'link-rel-noopener': true,
+		'no-bare-strings': true,
+		'no-html-comments': true,
+		'no-inline-styles': true,
+		'no-invalid-interactive': true,
+		'no-nested-interactive': true,
+		'no-triple-curlies': true,
+		'no-unused-block-params': true,
+		'self-closing-void-elements': true,
+		'simple-unless': false,
+		'style-concatenation': true
+	},
+	ignore: [
+		'tests/dummy/app/**'
+	]
 };
