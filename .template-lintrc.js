5d051db7425a0e82b18c55ce1756aee5e741f373
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2019-04-10 16:30:02 +0200

    chore(files): update common files

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,24 +3,21 @@
 module.exports = {
 	extends: 'recommended',
 	rules: {
-		'block-indentation': 'tab',
-		'deprecated-each-syntax': true,
-		'deprecated-inline-view-helper': true,
-		'img-alt-attributes': true,
-		'inline-link-to': true,
-		'link-rel-noopener': true,
 		'no-bare-strings': true,
+		'block-indentation': 'tab',
 		'no-html-comments': true,
-		'no-inline-styles': true,
-		'no-invalid-interactive': true,
 		'no-nested-interactive': true,
-		'no-triple-curlies': true,
-		'no-unused-block-params': true,
 		'self-closing-void-elements': true,
-		'simple-unless': false,
-		'style-concatenation': true
-	},
-	ignore: [
-		'tests/dummy/app/**'
-	]
+		'no-triple-curlies': true,
+		'deprecated-each-syntax': true,
+		'link-rel-noopener': true,
+		'no-invalid-interactive': true,
+		'img-alt-attributes': true,
+		'style-concatenation': true,
+		'deprecated-inline-view-helper': true,
+		'no-unused-block-params': true,
+		'inline-link-to': true,
+		'no-inline-styles': true,
+		'simple-unless': false
+	}
 };
